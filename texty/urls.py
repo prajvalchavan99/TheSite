1ef32999fe8d0d54cf92df99a028d02618de4811
Prajval Chavan <prajvalchavan99@gmail.com>
2022-07-05 23:02:38 +0530

    First commit

--- texty/urls.py
+++ texty/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.Home,name="index"),
+    path('text-compare/', views.textcompare,name="textcompare"),
 ]
--- texty/views.py
+++ texty/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def Home(request):
     return render(request,'index.html')
+
+def textcompare(request):
+    return render(request,'textcompare.html')
