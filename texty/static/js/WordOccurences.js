350083d159fd2528da5d5a3a16e4b95e7f70d4f8
Prajval Chavan <prajvalchavan99@gmail.com>
2022-12-16 21:50:20 +0530

    partial working

---
+++ texty/static/js/WordOccurences.js
@@ -0,0 +1,11 @@
+'use strict';
+const e=React.createElement;
+
+class WordOccurences extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            list:[],
+        }
+    }-
+}
--- texty/views.py
+++ texty/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def Home(request):
-    return render(request,'index.html')
+    context={}
+    context['openpopup']=True
+    return render(request,'index.html',context)
 
 def textcompare(request):
     return render(request,'textcompare.html')
